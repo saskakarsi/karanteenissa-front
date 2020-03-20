39049abce41714b5f10f6c9bbde671755a735524
Saska Karsi <saska.karsi@gmail.com>
2020-03-21 01:38:54 +0200

    Actually add home

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p class="text-center">
+            Important information and stuff about the coronavirus <br>
+            check if this is legit<br>
+            <a href="https://www.worldometers.info/coronavirus/" target="_blank">Worldometers statistics</a>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({})
+}
+</script>
