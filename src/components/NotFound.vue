088d07f6ca3f74004bba63e6711754661e50e06b
Saska Karsi <saska.karsi@gmail.com>
2020-03-20 23:23:46 +0200

    Actually save notfound

---
+++ src/components/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> 
+    <h1 class="text-center">Oops.</h1>
+    <p class="text-center"> 
+        Content not found. 
+        Return to <a href="/">home</a>?
+    </p> 
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NotFound',
+  }
+</script>
