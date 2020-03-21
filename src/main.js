4f7a8626951e4c951be0c7488ab5e788e619fd0e
Saska Karsi <saska.karsi@gmail.com>
2020-03-21 22:03:43 +0200

    Add trackers

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import VueGtag from "vue-gtag";
+
 Vue.config.productionTip = false
+
+
+Vue.use(VueGtag, {
+  config: { id: 'UA-160939788-1' }
+});
 
 new Vue({
   router,
