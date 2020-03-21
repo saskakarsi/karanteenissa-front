fccb1d8495b9d66c52a1a21fe496089bb5e75a89
Saska Karsi <saska.karsi@gmail.com>
2020-03-21 22:55:53 +0200

    Renames

--- src/util/state.js
+++ src/util/state.js
@@ -5,18 +5,18 @@ const state = Vue.observable({
         lang: 'Suomi',
         value: 'fi'
     },
-    selectedService: undefined,
+    selectedCategory: undefined,
     selectedLocation: undefined
 })
 
 export const getters = {
     locale: () => state.locale,
-    selectedService: () => state.selectedService,
+    selectedCategory: () => state.selectedCategory,
     selectedLocation: () => state.selectedLocation
 }
 export const mutators = {
     setLocale: (val) => state.locale = val,
-    setSelectedService: (val) => state.selectedService = val,
+    setselectedCategory: (val) => state.selectedCategory = val,
     setSelectedLocation: (val) => state.selectedLocation = val
 }
 
