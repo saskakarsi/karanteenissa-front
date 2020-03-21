8c67b2094ab319f5d9d2704d08428aa88d022dea
ohtopen <47524838+ohtopen@users.noreply.github.com>
2020-03-22 01:02:09 +0200

    add localizations

--- src/fixtures/locales.js
+++ src/fixtures/locales.js
@@ -16,6 +16,9 @@ const menuLocalizations = {
         app: {
             serviceButton: 'Palvelut',
             contactButton: 'Ota yhteyttä',
+            mainTitle: 'Löydä apua karanteenissa',
+            frontTitle1: 'Karanteenissa.fi kokoaa tietoa erilaisista tuote- ja palveluratkaisuista koronaviruksen vaikutusten ehkäisemiseksi.',
+            frontTitle2: 'Pidetään toisistamme huolta. <3'
         },
         services: {
             categorySelector: 'Kategoria',
@@ -31,7 +34,10 @@ const menuLocalizations = {
     se: {
         app: {
             serviceButton: 'Tjänster',
-            contactButton: 'Kontakta oss'
+            contactButton: 'Kontakta oss',
+            mainTitle: 'Hjälp för din karantän',
+            frontTitle1: 'Karanteenissa.fi samlar information om olika produkt- och servicelösningar för att förebygga coronavirusets verkningar.',
+            frontTitle2: 'Låt oss ta hand om varandra. <3'
         },
         services: {
             categorySelector: 'Kategori',
@@ -47,7 +53,10 @@ const menuLocalizations = {
     gb: {
         app: {
             contactButton: 'Contact us',
-            serviceButton: 'Services'
+            serviceButton: 'Services',
+            mainTitle: 'Find help in quarantine',
+            frontTitle1: 'Karanteenissa.fi combines information about different services to minimize the effects of the Corona virus',
+            frontTitle2: 'Let us take care of each other. <3'
         },
         services: {
             categorySelector: 'Category',
