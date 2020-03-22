c59e503238069961360566ed5e685b76f4a7475f
Saska Karsi <saska.karsi@gmail.com>
2020-03-22 04:17:01 +0200

    Fix home button text in mobile

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@
                 <v-icon>mdi-home</v-icon>
               </v-list-item-icon>
               <v-list-item-content>
-                <v-list-item-title>{{ menuTexts.serviceButton }}</v-list-item-title>
+                <v-list-item-title>{{ menuTexts.homeButton }}</v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item href="#/services">
