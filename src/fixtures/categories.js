5f5d8c3b1b19403b7c7552b8d3d9ac64f210050f
ohtopen <47524838+ohtopen@users.noreply.github.com>
2020-03-28 14:47:35 +0200

    fix localizations

--- src/fixtures/categories.js
+++ src/fixtures/categories.js
@@ -2,8 +2,50 @@ const categories = [
     {
         name: {
             fi: 'Ruoka',
-            se: 'Mat',
+            se: 'Food',
             gb: 'Food'
+        }
+    },
+    {
+        name: {
+            fi: 'Palvelut',
+            se: 'Services',
+            gb: 'Services'
+        }
+    },
+    {
+        name: {
+            fi: 'Verkkokauppa',
+            se: 'Ecommerce',
+            gb: 'Ecommerce'
+        }
+    },
+    {
+        name: {
+            fi: 'Hoiva',
+            se: 'Care',
+            gb: 'Care'
+        }
+    },
+    {
+        name: {
+            fi: 'Kodit',
+            se: 'Homes',
+            gb: 'Homes'
+        }
+    },
+    {
+        name: {
+            fi: 'Urheilu',
+            se: 'Sports',
+            gb: 'Sports'
+        }
+    },
+    {
+        name: {
+            fi: 'Viihde',
+            se: 'Entertainment',
+            gb: 'Entertainment'
         }
     }
 ]
